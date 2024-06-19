c45b5e1a6d0a0c32e56133697a6b88d72c3e8e82
Vitoria-Daris-Bender <vitoria-bender1@educar.rs.gov.br>
2024-06-19 10:05:56 -0300

    commit?

---
+++ src/ExemplosAula20/ExemploAcesso/Acesso.java
@@ -0,0 +1,4 @@
+package ExemplosAula20.ExemploAcesso;
+
+public class Acesso {
+}
---
+++ src/ExemplosAula20/Operacoes.java
@@ -0,0 +1,15 @@
+package ExemplosAula20;
+
+public class Operacoes {
+    static int soma(int n1, int n2){
+        return n1+n2;
+    }
+
+    static float potencia(float base, float expoente){
+        float mult = base;
+        for(int i=0; i<expoente; i++){
+            mult*=base;
+        }
+        return mult;
+    }
+}
